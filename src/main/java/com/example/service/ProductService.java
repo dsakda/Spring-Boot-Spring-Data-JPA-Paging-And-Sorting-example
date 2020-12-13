39d2b6bbf42d6f124cb02d00af8ba4ee74001eec
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-14 05:34:13 +0700

    Update ProductService.java

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -3,6 +3,9 @@ package com.example.service;
 import com.example.model.Product;
 import com.example.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -15,8 +18,14 @@ public class ProductService {
     @Autowired
     private ProductRepository repo;
 
-    public List<Product> listAll() {
-        return repo.findAll();
+//    public List<Product> listAll() {
+//        return repo.findAll();
+//    }
+
+    public Page<Product> listAll(int pageNum) {
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNum - 1, pageSize);
+        return repo.findAll(pageable);
     }
 
     public void save(Product product) {
