ef553c35cc0363a883926b602f2a0af3ce24a762
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-16 03:33:06 +0700

    Update ProductRepository.java

--- src/main/java/com/example/repository/ProductRepository.java
+++ src/main/java/com/example/repository/ProductRepository.java
@@ -2,6 +2,10 @@ package com.example.repository;
 
 import com.example.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
