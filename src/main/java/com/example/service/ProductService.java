fa25cc8107cb73cd198a7b4ba2805c146feab42b
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-25 01:31:11 +0700

    Update ProductService.java

--- src/main/java/com/example/service/ProductService.java
+++ src/main/java/com/example/service/ProductService.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 @Service
 @Transactional
